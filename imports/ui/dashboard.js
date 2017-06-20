068135e86e548cdfada81aece470cd5ce7de4f5c
Paul Peterson <doghouse308@yahoo.com>
2017-06-20 10:48:39 -0400

    setup mobile navigation

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -7,7 +7,7 @@ import Editor from './editor';
 export default () => {
   return (
     <div>
-      <PrivateHeader title="Dashboard" />
+      <PrivateHeader title="Notes" />
       <div className="page-content">
         <div className="page-content__sidebar">
           <NoteList />
